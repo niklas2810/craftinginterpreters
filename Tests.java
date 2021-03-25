522fbcbb1d1649e2947f14a78623d2965d1134e9
Niklas Arndt <git@niklasarndt.com>
2021-03-25 13:08:25 +0100

    Reformat code and run tests multiple time

--- Tests.java
+++ Tests.java
@@ -1,28 +1,31 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
+import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Tests {
 
-  public static void main(String[] args) {
-    new Tests().execute();
-  }
+  public static void main(String[] args) { new Tests().execute(); }
+
+  private static final int TEST_RUNS = 75;
 
   private final File cwd = new File(".");
   private final File testDir = new File("tests");
   private final Map<String, String> testers = new HashMap<>();
 
   public void execute() {
-    if(!testDir.exists() || !testDir.isDirectory()) {
-      System.err.println("Can not read from test directory " + testDir.getAbsolutePath());
+    if (!testDir.exists() || !testDir.isDirectory()) {
+      System.err.println("Can not read from test directory " +
+                         testDir.getAbsolutePath());
       System.exit(1);
     }
     registerJloxTester();
 
-    System.out.println(String.format("Found %d performance test runners", testers.size()));
+    System.out.println(
+        String.format("Found %d performance test runners", testers.size()));
+    testers.forEach((name, command) -> System.out.printf("Using runner %s with \"%s\"\n", name, command));
     runPerformanceTests();
   }
 
@@ -30,63 +33,89 @@ public class Tests {
     String path = "jlox.jar";
     File location = new File(path);
 
-    if(!location.exists()) {
-      path = String.format("jlox%starget%sjlox.jar", File.separator, File.separator);
+    if (!location.exists()) {
+      path = String.format("jlox%starget%sjlox.jar", File.separator,
+                           File.separator);
       location = new File(path);
 
-      if(!location.exists()) {
-        System.err.println("File jlox.jar is not present in current directory or in jlox/target/");
+      if (!location.exists()) {
+        System.err.println(
+            "File jlox.jar is not present in current directory or in jlox/target/");
         System.exit(1);
       }
     }
 
-    System.out.println("Using jar at location " + location.getAbsolutePath());
-    testers.put("Java", "java -jar " +  path);
+    testers.put("Java", "java -jar " + path);
   }
 
   private void runPerformanceTests() {
+    System.out.println("=".repeat(50));
+    for (File f : testDir.listFiles((dir, name) -> name.endsWith(".lox"))) {
+      String shortName = f.getName().substring(0, f.getName().indexOf(".lox"));
+      String prompt = "-".repeat((43 - shortName.length()) / 2) +
+                      " FILE: " + shortName + " ";
+      System.out.println(prompt + "-".repeat(50 - prompt.length()));
+
+      testers.forEach((name, cmd) -> {
+        long sum = 0;
+        long min = Long.MAX_VALUE;
+        long max = Long.MIN_VALUE;
+
+        for (int i = 0; i < TEST_RUNS; ++i) {
+          long curr = runTestCase(f, cmd);
+          sum += curr;
+          if (curr < min)
+            min = curr;
+          if (curr > max)
+            max = curr;
+        }
+
+        long average = sum / TEST_RUNS;
+
+        System.out.printf("%s: %dms on average (%d-%dms in %d test runs)\n",
+                          name, average, min, max, TEST_RUNS);
+      });
+
       System.out.println("=".repeat(50));
-      for(File f : testDir.listFiles((dir, name) -> name.endsWith(".lox"))) {
-          String shortName= f.getName().substring(0, f.getName().indexOf(".lox"));
-          String prompt = "-".repeat((43-shortName.length())/2) + " FILE: " + shortName + " ";
-          System.out.println(prompt + "-".repeat(50-prompt.length()));
+    }
+  }
 
-          testers.forEach((name, cmd) -> {
-            long start = System.currentTimeMillis();
-            try {
-              Process p = Runtime.getRuntime().exec(cmd + " ./tests/" + f.getName(), new String[0], cwd);
-              p.waitFor();
-              if(p.exitValue() != 0) {
-                System.err.println("ERROR: " + name + " returned " + p.exitValue() + " for " + shortName);
-                System.err.println("Application Output:");
-                try(BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
-                  String line;
+  private long runTestCase(File f, String cmd) {
+    long start = System.currentTimeMillis();
+    try {
+      Process p = Runtime.getRuntime().exec(cmd + " ./tests/" + f.getName(),
+                                            new String[0], cwd);
+      p.waitFor();
+      if (p.exitValue() != 0) {
+        System.err.println("ERROR: " + cmd + " returned " + p.exitValue() +
+                           " for " + f.getName());
+        System.err.println("Application Output:");
+        try (BufferedReader reader = new BufferedReader(
+                 new InputStreamReader(p.getInputStream()))) {
+          String line;
 
-                  while((line = reader.readLine()) != null)
-                    System.out.println(line);
-                } catch (IOException e) {
-                  System.err.println("Could not read application standard output");
-                }
+          while ((line = reader.readLine()) != null)
+            System.out.println(line);
+        } catch (IOException e) {
+          System.err.println("Could not read application standard output");
+        }
 
-                try(BufferedReader reader = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
-                  String line;
+        try (BufferedReader reader = new BufferedReader(
+                 new InputStreamReader(p.getErrorStream()))) {
+          String line;
 
-                  while((line = reader.readLine()) != null)
-                    System.out.println(line);
-                } catch (IOException e) {
-                  System.err.println("Could not read application error output");
-                }
-                System.exit(1);
-              }
-            } catch(Exception e) {
-              System.out.print("[FAILED due to " + e.getClass().getSimpleName() + "] ");
-            }
-            long end = System.currentTimeMillis();
-
-            System.out.println(name + ": " + (end-start) + "ms");
-          });
-
-          System.out.println("=".repeat(50));
+          while ((line = reader.readLine()) != null)
+            System.out.println(line);
+        } catch (IOException e) {
+          System.err.println("Could not read application error output");
+        }
+        System.exit(1);
       }
+    } catch (Exception e) {
+      System.out.print("[FAILED due to " + e.getClass().getSimpleName() + "] ");
+    }
+    long end = System.currentTimeMillis();
+
+    return end - start;
   }
 }
